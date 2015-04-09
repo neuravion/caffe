d06aa9e578ddb361143f2c8f2959badc99c2eea1
Karl Ridgeway <kridgeway@sensoryinc.com>
2015-04-09 09:30:37 -0600

    Fixing bad merge

--- src/caffe/util/upgrade_proto.cpp
+++ src/caffe/util/upgrade_proto.cpp
@@ -828,6 +828,10 @@ bool UpgradeV1LayerParameter(const V1LayerParameter& v1_layer_param,
     layer_param->mutable_loss_param()->CopyFrom(
         v1_layer_param.loss_param());
   }
+  if(v1_layer_param.has_stats_param() ) {
+    layer_param->mutable_stats_param()->CopyFrom(
+        v1_layer_param.stats_param() );
+  }
   if (v1_layer_param.has_layer()) {
     LOG(ERROR) << "Input NetParameter has V0 layer -- ignoring.";
     is_fully_compatible = false;
